224e2f5b0b1a4bfac06cbdc3e2a524889a17ffde
Mateja Trifunovski <mtrifunovski@gmail.com>
2018-09-08 13:18:41 +0200

    Extract views to dashboard component

---
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { etherscanLink } from '../../services/utils';
+import ContractHome from '../ContractHome/ContractHome';
+
+import './Dashboard.scss';
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 'alerts',
+    };
+
+    this.setActiveTab = this.setActiveTab.bind(this);
+  }
+
+  setActiveTab(activeTab) {
+    this.setState({
+      activeTab,
+    });
+  }
+
+  render() {
+    const { activeTab } = this.state;
+    const { contractAddress = '0x' } = this.props;
+    return (
+      <div className="contract-home-wrapper">
+        <div className="nav">
+          <div className="container">
+            <div className="logo-wrapper">
+              <Link className="logo" to="/">SmartAlert</Link>
+              <div className="tabs">
+                <div onClick={() => this.setActiveTab('alerts')}
+                     className={activeTab === 'alerts' ? 'tab active' : 'tab'}>Alerts
+                </div>
+                <div onClick={() => this.setActiveTab('stats')}
+                     className={activeTab === 'stats' ? 'tab active' : 'tab'}>Statistics
+                </div>
+              </div>
+            </div>
+            <a
+              className="ct-address"
+              href={etherscanLink(contractAddress)}
+              target="_blank" rel="noopener"
+            >{contractAddress}</a>
+          </div>
+        </div>
+
+        {
+          activeTab === 'alerts' &&
+          <ContractHome />
+        }
+        {
+          activeTab === 'stats' &&
+          <div>
+
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
